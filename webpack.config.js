ba6c6321063f8dec857f26402da4b2a86373f9fa
Viktor Shcheglov <vscheglov@odin.com>
2017-05-31 18:48:21 +0300

    env

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './app.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      },
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: '!!html-loader!./index.html',
+    }),
+  ],
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 8181,
+    host: '0.0.0.0',
+    disableHostCheck: true,
+  },
+};
